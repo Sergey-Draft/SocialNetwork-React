1ded29bad053069466438136b5f630c439825404
Sergey-Draft <drfatkrot1987@gmail.com>
2021-08-05 09:07:49 +0300

    users page fix

--- src/components/redux/sideBarReducer.js
+++ src/components/redux/sideBarReducer.js
@@ -1,17 +1,19 @@
-import img from "../../img/user-3.jpg"
+import avatar1 from "../../img/user-3.jpg";
+import avatar2 from '../../img/user2.jpg';
+import avatar3 from '../../img/user3.jpg';
 
 let initialState = {
     friends: [
-        { id: 1, imgurl:  img},
-        { id: 2, imgurl: "https://st3.depositphotos.com/1432405/19346/v/950/depositphotos_193462432-stock-illustration-businessman-icon-flat-style.jpg" },
-        { id: 3, imgurl: "https://st3.depositphotos.com/1432405/19346/v/950/depositphotos_193462432-stock-illustration-businessman-icon-flat-style.jpg" },
+        { id: 1, imgurl: avatar1 },
+        { id: 2, imgurl: avatar2 },
+        { id: 3, imgurl: avatar3 },
         { id: 4, imgurl: "https://st3.depositphotos.com/1432405/19346/v/950/depositphotos_193462432-stock-illustration-businessman-icon-flat-style.jpg" },
         { id: 5, imgurl: "https://st3.depositphotos.com/1432405/19346/v/950/depositphotos_193462432-stock-illustration-businessman-icon-flat-style.jpg" }
-      ]
+    ]
 }
 
 
-const sideBarReducer = (state=initialState, action) => {
+const sideBarReducer = (state = initialState, action) => {
 
     return state;
 }
